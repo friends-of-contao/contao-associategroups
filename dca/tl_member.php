0d37de9feb5ce51cdf04d1dfc6a22a4d5bdf2cd9
Kirsten Roschanski <kirsten@kat-webdesign.de>
2013-02-20 22:15:34 +0100

    Synchronizes the members,users to groups in the intermediate table by action.

--- dca/tl_member.php
+++ dca/tl_member.php
@@ -34,3 +34,13 @@
 $GLOBALS['TL_DCA']['tl_member']['config']['ondelete_callback'][] = array('AssociateGroups', 'deleteGroups');
 $GLOBALS['TL_DCA']['tl_member']['fields']['groups']['save_callback'][] = array('AssociateGroups', 'saveGroups');
 
+/**
+ * List
+ */
+$GLOBALS['TL_DCA']['tl_member']['list']['global_operations']['sync_associate_groups'] = array
+(
+	'label'				=> &$GLOBALS['TL_LANG']['tl_member']['sync_associate_groups'],
+	'href'				=> 'key=sync_tl_member_to_group',
+	'class'				=> 'sync_associate_groups header_sync_all',
+	'attributes'		=> 'onclick="Backend.getScrollOffset();"'
+);
