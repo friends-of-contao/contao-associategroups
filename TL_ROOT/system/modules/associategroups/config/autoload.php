146b416ca6e63b78992e3fb53c4db9a4c18db945
Oliver Hoff <oliver@hofff.com>
2013-09-18 23:34:30 +0200

    contao 3 compat (thanks to cliffparnitzky)

---
+++ TL_ROOT/system/modules/associategroups/config/autoload.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2013 Leo Feyer
+ *
+ * @package Associategroups
+ * @link    https://contao.org
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+
+/**
+ * Register the classes
+ */
+ClassLoader::addClasses(array
+(
+	'AssociateGroups' => 'system/modules/associategroups/AssociateGroups.php',
+));
