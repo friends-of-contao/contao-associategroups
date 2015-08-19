5aa411f90ab651bccbbe8ccc14f4bc72cad2ff4f
Cliff Parnitzky <github@cliff-parnitzky.de>
2015-08-19 23:16:52 +0200

    Renaming html to default assets folder

--- TL_ROOT/system/modules/associategroups/config/config.php
+++ TL_ROOT/system/modules/associategroups/config/config.php
@@ -7,4 +7,4 @@ $GLOBALS['BE_MOD']['accounts']['member']['sync_tl_member_to_group']
 $GLOBALS['BE_MOD']['accounts']['user']['sync_tl_user_to_group']
 	= array('AssociateGroups', 'syncUserToGroup');
 
-TL_MODE == 'BE' && $GLOBALS['TL_CSS'][] = 'system/modules/associategroups/html/backend.css';
+TL_MODE == 'BE' && $GLOBALS['TL_CSS'][] = 'system/modules/associategroups/assets/backend.css';
